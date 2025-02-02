e12d882ea94c2a20bcfbb6f6111f89e111b124f5
KozloVXII <a.kozlov@stanexim.local>
2025-02-02 04:24:18 +0300

    II

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <iostream>
 #include "ConverterJSON.h"
 
 extern std::vector<std::string> listRequests;
@@ -13,7 +14,6 @@ int main(int argc, char **argv){
 
     ConverterJSON converter;
     InvertedIndex invertedIndex;
-
     std::vector<std::string> textFiles;
 
     auto fileAnswers = new std::ofstream("answers.json",std::ios_base::trunc | std::ios_base::out);
@@ -21,19 +21,20 @@ int main(int argc, char **argv){
 
     std::string versionEngine = "0.1.001";
     nlohmann::json jsonConfig;
-    std::ifstream configFile("config.json", std::ios::in);
-
+    std::ifstream configFile("config.json");
     try
     {
-        if (!configFile.is_open())
+        if (!configFile.is_open()) {
             throw "Config file is missing";
+          }
         configFile >> jsonConfig;
         configFile.close();
-        if (!jsonConfig["config"].is_object())
+        if (!jsonConfig["config"].is_object()) {
             throw "Config file is empty";
-        if (jsonConfig["config"]["version"]!=versionEngine)
+          }
+        if (jsonConfig["config"]["version"]!=versionEngine){
             throw "\"config.json\" has incorrect file version.";
-
+          }
     }
     catch (const char* errorMessage)
     {
@@ -50,9 +51,10 @@ int main(int argc, char **argv){
     converter.putAnswers(listAnswers);
 
 
-    if(!gTest) return 0;
-
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    if(gTest) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+      }
+    return 0;
 }
 
